a81cce87e5aa8b6e9501753e9c6ff22c5631526c
Bruce Cohen <bruce@bikemd.org>
2019-04-15 09:49:53 -0400

    now with comments

--- node/app.js
+++ node/app.js
@@ -2,6 +2,7 @@
 // node --polyglot --jvm --vm.cp=/Users/bcohen/Documents/code/graal-demo/converter/target/classes app.js
 var mongodb = require('mongodb');
 
+// create an instance of the Java Converter class
 var ConverterClass = Java.type("org.example.Converter");
 var converter = new ConverterClass();
 
@@ -24,17 +25,20 @@ client.connect(function(err) {
      for(var i=0; i < documents.length; i++) {
        var doc = documents[i];
 
+       console.log(JSON.stringify(doc, 0, 2));
+
+       // create an instance of a Java HashMap to use as an argument to the
+       // Java Converter class
        var HashMap = Java.type('java.util.HashMap');
        var map = new HashMap();
        map.put("first", doc.first);
        map.put("last", doc.last);
 
+       // call the convertToJSON on the Java converter class
        var personJSON = converter.convertToJSON(map);
        var person = JSON.parse(personJSON);
 
        console.log(JSON.stringify(person, 0, 2));
-
-       console.log(JSON.stringify(doc));
     }
  })
  client.close();
